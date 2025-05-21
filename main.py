2f820813616d137681dbbd2f2a24883243e6276b
Enzo Gabriel <160799288+enzogabrielS@users.noreply.github.com>
2025-05-20 22:10:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ async def on_raw_reaction_add(payload):
 
 
 @bot.command()
-async def ola(ctx: commands.Context):
+async def Hello(ctx: commands.Context):
     mention = ctx.author.mention
     server = ctx.guild
     await ctx.reply(f"Hello, user {mention}, by {server}")
@@ -35,10 +35,6 @@ async def ola(ctx: commands.Context):
 async def falar(ctx: commands.Context,*, texto):
     await ctx.reply(texto)
 
-@bot.command()
-async def somar(ctx: commands.Context,num1:int, num2:int):
-    result = num1 + num2
-    await ctx.reply(f"a soma de {num1} e {num2}, sera igual a {result}.")
 
 @bot.command()
 async def clear(ctx: commands.Context, quantidade:int = 100, limite:int = 1000):
@@ -59,23 +55,6 @@ async def clear(ctx: commands.Context, quantidade:int = 100, limite:int = 1000):
     minha_embed.description = f"{mensagens_apagaveis} mensagens foram apagadas"
     await ctx.send(embed=minha_embed, delete_after=10)
 
-@bot.command()
-async def pix(ctx: commands.Context):
-    async def replybutton(interact: discord.Interaction):
-        await interact.response.send_message("Pix: 8aa8a7c5-7ba8-4a80-996b-2c0e27b00486", ephemeral=True, delete_after=60)
-
-    view = discord.ui.View()
-    button = discord.ui.Button(label="🛒", style=discord.ButtonStyle.green)
-    button_url = discord.ui.Button(label="@ezzgabb", style=discord.ButtonStyle.grey, url="https://www.instagram.com/ezzgabb/")
-    button.callback = replybutton
-
-    view.add_item(button)
-    view.add_item(button_url)
-    await ctx.reply(view=view)
-
-@bot.command()
-async def status(ctx: commands.Context):
-    await bot.change_presence(status=discord.Status.idle, activity=discord.Game("@ezzgabb"))
 
 @bot.command()
 async def spam(ctx: commands.Context, mensagem, quantidade: int):
@@ -92,5 +71,9 @@ async def spam(ctx: commands.Context, mensagem, quantidade: int):
 
     except Exception as e:
         await ctx.reply(f"Ocorreu um erro: {e}", ephemeral=True, delete_after=10)
+
+@bot.command()
+async def status(ctx: commands.Context):
+    await bot.change_presence(status=discord.Status.idle, activity=discord.Game("@ezzgabb"))
 
 bot.run("")
